16473cb03894f45b98b4b2eacecca64d30e271b0
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-19 12:26:52 +0900

    乱数生成部分追加

--- sort/sorting.c
+++ sort/sorting.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>  /* random, srandom */
+#include <time.h>    /* time */
+
+void gendata(int a[], int n, int w)
+{
+  int i;
+  for(i = 0; i < n; i++)
+    a[i] = random() % w;
+}
+
+int main(int argc, char *argv[])
+{
+  int datanum = 10, width = 5;
+  int data[datanum];
+  int i;
+
+  srandom(time(NULL)); /* initialize random */
+
+  gendata(data, datanum, width);
+
+  
+  for(i = 0; i < datanum; i++)
+    printf("%d\n", data[i]);
+
+
+  return 0;
+}
