64533cb3c13e74127981b5038372e43d7bd5c11e
KohheiAdachi <d13301@yonago.kosen-ac.jp>
2018-06-21 01:21:41 +0900

    データ追加

--- sort/sorting.c
+++ sort/sorting.c
@@ -50,8 +50,9 @@ void insertion_sort(element_type data[],int n){
 }
 int main(int argc, char *argv[])
 {
-  int datanum,width = 100000;
-
+  int datanum =100000 ,width = 100000;
+  int data[datanum];
+  int data_b[datanum],data_s[datanum],data_i[datanum];
   int i;
 
   srandom(time(NULL)); /* initialize random */
@@ -60,24 +61,38 @@ int main(int argc, char *argv[])
 
   for(i=1;i<=10;i++){
     datanum = i * 10000;
-    int data[datanum];
-    int data_b[datanum],data_s[datanum],data_i[datanum];
-    printf("data: %d\n",datanum);
+    printf("%d,",datanum);
     gendata(data, datanum, width);
     //配列のコピー
     memcpy(data_s, data, sizeof(int) * datanum);
     memcpy(data_i, data, sizeof(int) * datanum);
+
+/*
   bubble_sort(data,datanum);
   cpu_b = (double)(e - s)/CLOCKS_PER_SEC;
   printf("bubble_sort: %f\n",cpu_b);
-
   selection_sort(data_s,datanum);
   cpu_s = (double)(e - s)/CLOCKS_PER_SEC;
   printf("selection_sort: %f\n",cpu_s);
-
   insertion_sort(data_i,datanum);
   cpu_i = (double)(e - s)/CLOCKS_PER_SEC;
   printf("insertion_sort: %f\n",cpu_i);
+*/
+
+  bubble_sort(data,datanum);
+  cpu_b = (double)(e - s)/CLOCKS_PER_SEC;
+  printf("%f,",cpu_b);
+  selection_sort(data_s,datanum);
+  cpu_s = (double)(e - s)/CLOCKS_PER_SEC;
+  printf("%f",cpu_s);
+  insertion_sort(data_i,datanum);
+  cpu_i = (double)(e - s)/CLOCKS_PER_SEC;
+  printf(",%f\n",cpu_i);
+
+
+
+
+
 }
 
 /*
