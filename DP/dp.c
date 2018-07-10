9fa9f81c9acd8a7859ecd2c7e0491fa5bef72098
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-07-11 01:10:57 +0900

    dpのところ

--- DP/dp.c
+++ DP/dp.c
@@ -14,9 +14,37 @@ int r_comb(int n,int m)
 }
 
 int d_comb(int n,int m){
-  
+  int i,j;
+  int **matrix,*base_matrix;
+
+  matrix = malloc(sizeof(int *)*n);
+  base_matrix = malloc(sizeof(int)*n*n);
+
+  if(m == 0){
+    return 1;
+  }
+
+  for(i=0;i<n;i++){
+    matrix[x] = base_matrix + i * n;
+  }
+  for(i = 0;i<=m;i++){
+    matrix[0][i] = 1;
+  }
+  for(i = 0;i<=n-m;i++){
+    matrix[i][0] = 1;
+  }
+  for(i = 0;i <=n-m;i++){
+      for(j=1;j<=m;j++){
+        matrix[i][j] = matrix[i][j-1] + matrix[i-1][j];
+      }
+  }
+  return matrix[n-m][m];
+  free(matrix);
+  free(base_matrix);
+
 }
 
-int main (){
-
+int main(int argc, char const *argv[]) {
+  
+  return 0;
 }
