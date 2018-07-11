e16f3755a3ed7cf7ff1735a16fb00d5f7e4e4e26
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-07-11 15:31:19 +0900

    DP実装完了

--- DP/dp.c
+++ DP/dp.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
+#include <string.h>
 int count = 0;
 
 int r_comb(int n,int m)
@@ -18,14 +18,14 @@ int d_comb(int n,int m){
   int **matrix,*base_matrix;
 
   matrix = malloc(sizeof(int *)*n);
-  base_matrix = malloc(sizeof(int)*n*n);
+  base_matrix = malloc(sizeof(int) * n * n);
 
   if(m == 0){
     return 1;
   }
 
   for(i=0;i<n;i++){
-    matrix[x] = base_matrix + i * n;
+    matrix[i] = base_matrix + i * n;
   }
   for(i = 0;i<=m;i++){
     matrix[0][i] = 1;
@@ -33,7 +33,8 @@ int d_comb(int n,int m){
   for(i = 0;i<=n-m;i++){
     matrix[i][0] = 1;
   }
-  for(i = 0;i <=n-m;i++){
+
+  for(i = 1;i <= n - m; i++){
       for(j=1;j<=m;j++){
         matrix[i][j] = matrix[i][j-1] + matrix[i-1][j];
       }
@@ -45,6 +46,33 @@ int d_comb(int n,int m){
 }
 
 int main(int argc, char const *argv[]) {
-  
+
+  int n,m;
+  clock_t s,e;
+  double rt,dt;
+  if(argc != 2){
+  n = atoi(argv[1]);
+  m = atoi(argv[2]);
+
+  s = clock();
+  printf("r_comb:%d\n",r_comb(n,m));
+  e = clock();
+  rt = (double)(e-s)/CLOCKS_PER_SEC;
+  printf("count:%d\n",count);
+  printf("r_comb time:%lf\n",rt);
+
+  s = clock();
+  printf("d_comb:%d\n",d_comb(n,m));
+  e = clock();
+  dt = (double)(e-s)/CLOCKS_PER_SEC;
+  printf("d_comb time:%lf\n",dt);
+  }
+  else{
+    fprintf(stderr,"type:>%s n m \n",argv[0]);
+		return 1;
+  }
+
+
+
   return 0;
 }
